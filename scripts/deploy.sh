92ba4c74dbdecbc543364a83077a3249c12d6af4
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-08-29 18:59:43 +0530

    preserve travis.yml in master
    - the travis.yml in master is to avoid builds of master branch.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ git fetch --all origin
 git checkout -b master --track origin/master
 
 # Overwrite existing files with new files
-rsync -a --filter='P _site/' --filter='P _cache/' --filter='P .git/' --filter 'P .stack-work' --filter='P .gitignore' --delete-excluded _site/  .
+rsync -a --filter='P _site/' --filter='P .travis.yml' --filter='P _cache/' --filter='P .git/' --filter='P .stack-work' --filter='P .gitignore' --delete-excluded _site/  .
 
 # Commit
 git add -A
